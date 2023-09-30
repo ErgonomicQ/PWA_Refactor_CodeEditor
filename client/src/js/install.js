3b1273ce67eaaa4dcb4702865668e8c59024c1a8
ErgonomicQ <ergonomicQ@gmail.com>
2023-09-29 21:43:47 -0700

    configuation db and install.js, dist folder

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,25 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+let deferredPrompt;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  butInstall.style.display = 'block';
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+butInstall.addEventListener('click', async () => {
+  butInstall.style.display = 'none';
+  deferredPrompt.prompt();
+  const choiceResult = await deferredPrompt.userChoice;
+  if (choiceResult.outcome === 'accepted') {
+    console.log('User accepted the PWA installation');
+  } else {
+    console.log('User dismissed the PWA installation');
+  }
+  deferredPrompt = null;
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('App successfully installed');
+});
