723cf2dbaa79d62467437f5b3c99df38c12083ea
ErgonomicQ <ergonomicQ@gmail.com>
2023-09-30 00:24:58 -0700

    Readme final, and screenshot

--- README.md
+++ README.md
@@ -15,14 +15,22 @@
   Go to the posted link, then click on the install me button! 
   
   ## Usage
-  <LINK GOES HERE>
-  
+  [Visit the PWA App](https://pwa-app-module19-29ed7ef5b346.herokuapp.com)
+
+  ![The Text Editor Screenshot](screenshots/THE_TEXT_EDITOR.png)
+
+
   ## License
  This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
   
   ## Contributing
-  Initial Code was provided to me by my boot camp course, but all edits and additions are my own. 
-  
+  Initial Code was provided to me by my boot camp course. The putDB getDB functions found in the database.js were provided by the bootcamp as well, but contains comments of my own writing to explain what each line of the functions do as well as to show understanding for that function. Any other edits are my own. 
+
+IndexedDB. The Modern JavaScript Tutorial. (2022, June 20). https://javascript.info/indexeddb 
+
+MozDevNet. (n.d.). Progressive web apps. MDN. https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps 
+
+
   
   ## Questions
   if you have any questions, you can reach me @ [ErgonomicQ](https://github.com/ErgonomicQ) or shoot me email at c.s.cooke89@.gmail.com.
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,29 +12,29 @@ const initdb = async () =>
     },
   });
 
-// Method that takes some content and adds it to the IndexedDB database using the idb module
+// This method collects the on screen data and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
-  const result = await request;
-  console.log('Data saved to the database', result.value);
+  const db = await openDB('jate', 1); //this open a jate database with a ver 1
+  const tx = db.transaction('jate', 'readwrite'); //this is how we write the info to the index
+  const store = tx.objectStore('jate');//this access the store in the 'jate' object we made
+  const request = store.put({ id: 1, value: content });// this create a request, and stuffs the specified content in the object with an id of 1.
+  const result = await request; //waits for that to complete
+  console.log('Data saved', result); // And then logs a save message, ideally, with the result.
 };
 
 
 export const getDb = async () => {
   console.log('GET from the database');
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.get(1);
-  const result = await request;
-  result
+  const db = await openDB('jate', 1); //This *opens* the database with the version 1
+  const tx = db.transaction('jate', 'readonly'); // This starts a Readonly transaction on out 'jate' object
+  const store = tx.objectStore('jate');// access the 'jate' object from before
+  const request = store.get(1);// as above, create a request to get data with id 1 from the objects 'store'
+  const result = await request; //wait for the GET
+  result //if data is found, log the retreival message. otherwise through a 'not found' log
     ? console.log('Data retrieved from the database', result.value)
     : console.log('Data not found in the database');
-  return result?.value;
+  return result?.value; //return the data
 };
 
 initdb();
