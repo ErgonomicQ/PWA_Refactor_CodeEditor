32647d0cee57965a8f811a4202a6b533d920d158
ErgonomicQ <ergonomicQ@gmail.com>
2023-09-29 23:48:57 -0700

    attempting new code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,26 +15,25 @@ const initdb = async () =>
 // Method that takes some content and adds it to the IndexedDB database using the idb module
 export const putDb = async (content) => {
   console.log('PUT to the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('Data saved to the database', result.value);
 };
 
-// Method that gets content from the IndexedDB database using the idb module
+
 export const getDb = async () => {
   console.log('GET from the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
   result
     ? console.log('Data retrieved from the database', result.value)
     : console.log('Data not found in the database');
-  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
   return result?.value;
 };
 
