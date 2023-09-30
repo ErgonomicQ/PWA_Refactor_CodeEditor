3336509d59980863bf038ed37fe9727da57b8d38
ErgonomicQ <ergonomicQ@gmail.com>
2023-09-29 23:47:48 -0700

    attempting new code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,26 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Method that takes some content and adds it to the IndexedDB database using the idb module
 export const putDb = async (content) => {
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readwrite'); //allows data to be added
-  const store = tx.objectStore('jate');// where we'll be storing the data
-  const putRequest = store.put({id:1, value: content });// where we add the content to the 'jate' object.
-  const result = await putRequest;
-  console.log('Content added:', result);
-}
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('Data saved to the database', result.value);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
+// Method that gets content from the IndexedDB database using the idb module
 export const getDb = async () => {
-  console.log('GET content')
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readonly');
-  const store =  tx.objectStore('jate');
-  const content = store.get(1);
-  const results = await content 
-  console.log('Content loaded:', results);
-  return results;
-}
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('Data retrieved from the database', result.value)
+    : console.log('Data not found in the database');
+  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+  return result?.value;
+};
 
 initdb();
